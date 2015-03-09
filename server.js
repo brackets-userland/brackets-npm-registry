5cbbf478d14f39e0dbe7dab5e0ca038f882e4d47
Martin Zagora <zaggino@gmail.com>
2015-03-09 17:58:24 +1100

    make server rebuild registry every 15 minutes

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
+var fs = require('fs');
+var path = require('path');
 var registryBuilder = require('./dist/node/registry-builder');
 var express = require('express');
 var app = express();
+var filename = path.resolve(__dirname, 'registry.json');
 
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/registry', function (request, response, next) {
 
-  registryBuilder.buildRegistry()
-    .then(function (registry) {
-      response.send(registry);
-    })
-    .catch(function (err) {
+  fs.readFile(filename, { encoding: 'utf8' }, function (err, str) {
+
+    if (err) {
+      return next(err);
+    }
+
+    try {
+      response.send(JSON.parse(str));
+    } catch (err) {
       next(err);
-    });
+    }
+
+  });
 
 });
 
@@ -24,6 +33,21 @@ app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
 
+function rebuildRegistry () {
+  registryBuilder.buildRegistry()
+    .then(function (registry) {
+      fs.writeFileSync(filename, JSON.stringify(registry));
+      console.log('registry file refreshed');
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
+}
+
 setTimeout(function () {
-  registryBuilder.buildRegistry();
-}, 1000);
+  rebuildRegistry();
+}, 1);
+
+setInterval(function () {
+  rebuildRegistry();
+}, 15 * 60000);
