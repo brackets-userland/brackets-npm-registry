ee3f0fb9cd998ce837bbf8bdb3e9979ec207d9c3
Martin Zagora <zaggino@gmail.com>
2015-03-22 08:44:39 +1100

    refactor buildregistry

--- src/node/registry-builder.js
+++ src/node/registry-builder.js
@@ -1,86 +1,87 @@
-(function () {
-  'use strict';
+/*eslint strict:0*/
+'use strict';
 
-  let npm = require('npm');
-  let Promise = require('bluebird');
-  let request = require('request');
+const npm = require('npm');
+const Promise = require('bluebird');
+const request = require('request');
 
-  exports.buildRegistry = function () {
+function buildRegistry() {
 
-    let npmLoad = Promise.promisify(npm.load, npm);
-    return npmLoad()
-      .then(() => {
+  let npmLoad = Promise.promisify(npm.load, npm);
+  return npmLoad()
+    .then(() => {
 
-        // get all entries tagged 'brackets-extension'
-        let npmSearch = Promise.promisify(npm.commands.search, npm.commands);
-        return npmSearch(['brackets-extension'], true);
+      // get all entries tagged 'brackets-extension'
+      let npmSearch = Promise.promisify(npm.commands.search, npm.commands);
+      return npmSearch(['brackets-extension'], true);
 
-      })
-      .then(searchResults => {
+    })
+    .then(searchResults => {
 
-        // call view for all potential extensions
-        let npmView = Promise.promisify(npm.commands.view, npm.commands);
-        return Promise.all(Object.keys(searchResults).map(
-          extensionId =>
-            npmView([extensionId + '@latest'], true).then(result =>
-              result[Object.keys(result)[0]]
-            )
-        ));
+      // call view for all potential extensions
+      let npmView = Promise.promisify(npm.commands.view, npm.commands);
+      return Promise.all(Object.keys(searchResults).map(
+        extensionId =>
+          npmView([extensionId + '@latest'], true).then(result =>
+            result[Object.keys(result)[0]]
+          )
+      ));
 
-      })
-      .then(viewResults => {
+    })
+    .then(viewResults => {
 
-        // filter out those, which doesn't have brackets engine specified
-        return viewResults.filter(result => result.engines && result.engines.brackets);
+      // filter out those, which doesn't have brackets engine specified
+      return viewResults.filter(result => result.engines && result.engines.brackets);
 
-      })
-      .then(extensionInfos => {
+    })
+    .then(extensionInfos => {
 
-        // get download counts for the extensions
-        let extensionIds = extensionInfos.map(i => i.name);
-        let from = '2015-01-01';
-        let to = new Date().toISOString().substring(0,10);
-        return new Promise((resolve, reject) => {
-          request(`https://api.npmjs.org/downloads/range/${from}:${to}/${extensionIds.join(',')}`, (error, response, body) => {
+      // get download counts for the extensions
+      let extensionIds = extensionInfos.map(i => i.name);
+      let from = '2015-01-01';
+      let to = new Date().toISOString().substring(0, 10);
+      return new Promise((resolve, reject) => {
+        request(`https://api.npmjs.org/downloads/range/${from}:${to}/${extensionIds.join(',')}`,
+        (error, response, body) => {
 
-            if (error) {
-              return reject(error);
+          if (error) {
+            return reject(error);
+          }
+
+          if (response.statusCode !== 200) {
+            return reject(body);
+          }
+
+          if (typeof body === 'string') {
+            try {
+              body = JSON.parse(body);
+            } catch (err) {
+              return reject(err);
             }
+          }
 
-            if (response.statusCode !== 200) {
-              return reject(body);
-            }
-
-            if (typeof body === 'string') {
-              try {
-                body = JSON.parse(body);
-              } catch (err) {
-                return reject(err);
+          if (extensionIds.length === 1) {
+            extensionInfos[0].downloads = body.downloads;
+          } else {
+            extensionInfos.forEach(extensionInfo => {
+              let info = body[extensionInfo.name];
+              if (info && info.downloads) {
+                extensionInfo.downloads = info.downloads;
               }
-            }
+            });
+          }
 
-            if (extensionIds.length === 1) {
-              extensionInfos[0].downloads = body.downloads;
-            } else {
-              extensionInfos.forEach(extensionInfo => {
-                let info = body[extensionInfo.name];
-                if (info && info.downloads) {
-                  extensionInfo.downloads = info.downloads;
-                }
-              });
-            }
+          resolve(extensionInfos);
 
-            resolve(extensionInfos);
-
-          });
         });
       });
+    });
 
-  };
-
-}());
+}
 
 if (process.argv[1] === __filename) {
-  exports.buildRegistry(...process.argv.slice(2));
+  buildRegistry(...process.argv.slice(2));
+} else {
+  module.exports = buildRegistry;
 }
 
