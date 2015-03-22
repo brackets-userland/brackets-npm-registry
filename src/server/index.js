6e42c77aacb9c8771b14cc510503274aee17080c
Martin Zagora <zaggino@gmail.com>
2015-03-22 11:22:02 +1100

    refactor the server

--- src/server/index.js
+++ src/server/index.js
@@ -1,63 +1,59 @@
-var express = require('express');
-var app = express();
-var spawn = require('child_process').spawn;
-var fs = require('fs');
-var path = require('path');
-var filepath = path.resolve(__dirname, '../../tmp/registry.json');
+/*eslint strict:0, no-console:0, no-process-env:0*/
+'use strict';
 
-app.set('port', (process.env.PORT || 5000));
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const buffspawn = require('buffered-spawn');
+const nodeLookup = require('../node/node-lookup');
+const app = express();
+const registryFilePath = path.resolve(__dirname, '../../tmp/registry.json');
+const logger = function (...args) { console.log(...args); };
+
+const buildRegistry = function () {
+  nodeLookup(logger).then(nodePath => {
+
+    logger('going to build a registry.json file to', registryFilePath);
+
+    let args = ['../node/registry-builder.js', registryFilePath];
+
+    return buffspawn(nodePath, args, {
+      cwd: __dirname
+    }).progress(function (buff) {
+      logger('buildRegistry progress =>', buff.toString());
+    }).then(function () {
+      logger('registry file built at', registryFilePath);
+    });
+
+  }).catch(err => {
+    logger('failed to build a registry at', registryFilePath);
+    logger(err.name, ':', err.message, '\n', err.stack);
+  });
+};
+
+app.set('port', process.env.PORT || 5000);
 
 app.get('/registry', function (request, response, next) {
-
-  fs.readFile(filepath, { encoding: 'utf8' }, function (err, str) {
-
+  fs.readFile(registryFilePath, {encoding: 'utf8'}, function (err, str) {
     if (err) {
       return next(err);
     }
-
     try {
       response.send(JSON.parse(str));
-    } catch (err) {
-      next(err);
+    } catch (err2) {
+      next(err2);
     }
-
   });
-
 });
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
   response.send('Hello World!');
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+app.listen(app.get('port'), function () {
+  logger(`Node app is running at localhost:${app.get('port')}`);
+  // call on startup
+  setTimeout(buildRegistry, 1);
+  // and then every 30 minutes
+  setInterval(buildRegistry, 30 * 60000);
 });
-
-function callBuildScript() {
-
-  let args = ['build-script.js'];
-
-  console.log('calling: node', args.join(' '));
-
-  let buildScript = spawn('node', args, {
-    cwd: __dirname
-  });
-
-  buildScript.stdout.on('data', function (data) {
-    console.log('stdout: ' + data);
-  });
-
-  buildScript.stderr.on('data', function (data) {
-    console.log('stderr: ' + data);
-  });
-
-  buildScript.on('close', function (code) {
-    console.log('child process exited with code ' + code);
-  });
-
-}
-
-// call on startup
-setTimeout(callBuildScript, 1);
-// and then every 15 minutes
-setInterval(callBuildScript, 15 * 60000);
